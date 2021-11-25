4389b1e4e9c7dfdd8bdae67e08b74b0b7c5f7cb3
Matt Bentley <mbentley@mbentley.net>
2021-11-25 10:26:35 -0500

    Added jenkins agent
    Signed-off-by: Matt Bentley <mbentley@mbentley.net>

--- README.md
+++ README.md
@@ -5,6 +5,9 @@ docker image for jenkins
 To pull this image:
 `docker pull mbentley/jenkins`
 
+
+## Jenkins Controller
+
 Example usage:
 
 ```
@@ -17,3 +20,18 @@ docker run -d \
 ```
 
 Note: Jenkins runs as the user `jenkins` in the container.  The jenkins user uid & gid are `510` in case a volume is used for the data in `/var/lib/jenkins`.
+
+## Jenkins Agent
+
+Example usage:
+
+```
+docker run -d \
+  --name jenkins-agent \
+  -e JENKINS_URL="https://jenkins.example.com/" \
+  -e NODE_NAME="agent1" \
+  -e JENKINS_SECRET="myjenkinsnodesecretgoeshere" \
+  -e MAX_MEMORY="4g" \
+  -e JAVA_OPTS="-XX:+UseG1GC -XX:+UseStringDeduplication -XX:+ParallelRefProcEnabled -XX:+DisableExplicitGC -XX:+UnlockDiagnosticVMOptions -XX:+UnlockExperimentalVMOptions -verbose:gc -Xlog:gc" \
+   mbentley/jenkins:agent
+```
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,22 @@
 # variables that are inherited from the Dockerfile or passed at runtime
 JAVA_OPTS="${JAVA_OPTS:-}"
 MAX_MEMORY="${MAX_MEMORY:-}"
+JENKINS_URL="${JENKINS_URL:-}"
+NODE_NAME="${NODE_NAME:-}"
+JENKINS_SECRET="${JENKINS_SECRET:-}"
 
 case $1 in
+  jenkins-agent)
+    # make sure we have all of the required parameters
+    if [ -z "${JENKINS_URL}" ] || [ -z "${NODE_NAME}" ] || [ -z "${JENKINS_SECRET}" ]
+    then
+      echo "ERROR: required environment variable(s) missing: JENKINS_URL, NODE_NAME, and/or JENKINS_SECRET"
+      exit 1
+    fi
+
+    # shellcheck disable=SC2086
+    exec tini -- java "-Xmx${MAX_MEMORY}" "-Xms${MAX_MEMORY}" ${JAVA_OPTS} -jar /usr/share/jenkins/agent.jar -jnlpUrl "${JENKINS_URL}computer/${NODE_NAME}/jenkins-agent.jnlp" -secret "${JENKINS_SECRET}" -workDir "/var/lib/jenkins"
+    ;;
   jenkins)
     # shellcheck disable=SC2086
     exec tini -- java "-Xmx${MAX_MEMORY}" "-Xms${MAX_MEMORY}" ${JAVA_OPTS} -jar /usr/share/jenkins/jenkins.war
